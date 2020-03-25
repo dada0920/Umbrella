356ef43320cfc82b1cdcc64e7e1e8cac5b4dc7ee
dada0920 <dlekgud951753@naver.com>
2020-03-25 15:26:29 +0900

    dahyoung1-1

---
+++ dahyoung/umbrella2/main.py
@@ -0,0 +1,37 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import QtCore
+from PyQt5 import uic
+from PyQt5.QtCore import pyqtSlot, pyqtSignal, QUrl
+import re
+import datetime
+from UI.main_ui2 import Ui_MainWindow
+import pytube
+from threading import Thread
+from PyQt5.QtCore import *
+import subprocess
+
+
+class TestForm(QMainWindow, Ui_MainWindow) :
+    #생성자
+    def __init__(self) :
+        super().__init__() #부모의 생성자 함수 호출
+        self.url= "http://localhost:8080/umbrella"
+        self.setupUi(self)  # 초기화
+        self.webEngineView.load(QUrl(self.url))
+        self.pushButton.clicked.connect(self.aaa)
+        self.load.clicked.connect(self.daum_load)
+    def aaa(self) :
+        url = self.lineEdit.text().strip()
+        print('aaaaaaaa')
+        self.webEngineView.load(QUrl(url))
+    def daum_load(self) :
+        url="https://map.kakao.com/"
+        print('load')
+        self.webEngineView.load(QUrl(url))
+
+if __name__ == "__main__" :
+    app = QApplication(sys.argv)
+    window = TestForm()
+    window.show()
+    app.exec_()
