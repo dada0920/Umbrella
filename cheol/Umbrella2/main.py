d0aac95f263bbbd4a33b15157358e3b8b374a5b4
dada0920 <dlekgud951753@naver.com>
2020-03-25 15:51:55 +0900

    cheol 1-1 검색기능 위치X

--- cheol/Umbrella2/main.py
+++ cheol/Umbrella2/main.py
@@ -19,17 +19,17 @@ import subprocess
 sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = "utf-8")
 sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = "utf-8")
 
-#요청
-url="https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/storesByGeo/json"
-savename = "D:umbrella/Mask.xml"
-if not os.path.exists(savename):
-    req.urlretrieve(url,savename)
-
-#숲처리
-xml=open(savename,'r',encoding='utf-8').read()
-soup=BeautifulSoup(xml, "html.parser")
-
-print(soup.prettify())
+# #요청
+# url="https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/storesByGeo/json"
+# savename = "D:umbrella/Mask.xml"
+# if not os.path.exists(savename):
+#     request.urlretrieve(url,savename)
+#
+# #숲처리
+# xml=open(savename,'r',encoding='utf-8').read()
+# soup=BeautifulSoup(xml, "html.parser")
+#
+# print(soup.prettify())
 
 # 서치기능
 # - 약국 별
@@ -54,23 +54,16 @@ def SearchPharmacy(addr):
     else:
         return None
 
-#검색 결과 항목 정보 출력하기
-def showPharmacy(Pharmacy):
-    print("주소 : " +Pharmacy['addr'])
-    print("약국이름 : " +Pharmacy['name'])
-    print("위도 : ",Pharmacy['lat'])
-    print("경도 : ",Pharmacy['lng'])
-    if Pharmacy['created_at']==None :
-        print("정보 갱신 미상")
-    elif Pharmacy['created_at'] :
-        print("정보 갱신 시각 : "+Pharmacy['created_at'])
-    if Pharmacy['remain_stat']==None or Pharmacy['remain_stat']== 'break' or Pharmacy['remain_stat']=='empty':
-        print("마스크 재고 : 없음")
-    elif Pharmacy['remain_stat']=='some' or Pharmacy['remain_stat']=='few':
-        print("마스크 재고 : 부족")
-    elif Pharmacy['remain_stat']=='plenty':
-        print("마스크 재고: 충분")
-    print("================")
+
+
+#약국검색 메소드  data : jres["stores"], yg : 약국이름
+def searchYG(data, yg) :
+    for i in range(len(data)) :
+
+        if data[i].get("name") == yg :
+
+            return data[i]
+    return "약국 못찾음"
 
 #프로그램 진입점
 def main():
@@ -85,15 +78,68 @@ def main():
         print("json.loads 실패!!!")
         exit()
 
-    #검색 결과의 name 목록의 각 항목(post)을 출력
-    for post in jres['stores']:
-        showPharmacy(post)
+    yg_info = searchYG(jres["stores"], "역삼오늘약국")
+
+    print("약국 이름 : ",yg_info.get('name'))
+    print("주소 : ",yg_info.get('addr'))
+    if yg_info.get('created_at')=='empty' :
+        print("정보 갱신 미상")
+    elif yg_info.get('created_at') :
+        print("정보 갱신 시각 : ",yg_info.get('created_at'))
+    if yg_info.get('remain_stat')==None or yg_info.get('remain_stat')=='break' or yg_info.get('remain_stat')=='empty':
+        print("마스크 재고 : 없음")
+    elif yg_info.get('remain_stat')=='some' or yg_info.get('remain_stat')=='few':
+        print("마스크 재고 : 부족")
+    elif yg_info.get('remain_stat')=='plenty':
+        print("마스크 재고: 충분")
 
 # - 마스크 잔량 별
 #     ( 마스크 잔량 기준을 선택하면 해당 약국만 보여준다)
-#
+
+# 마스크 잔량 기준약국검색 메소드  data : jres["remain_stat"], mk : 마스크 잔량
+def searchMK(data, mk) :
+    list = []
+    for i in range(len(data)) :
+        if data[i].get("remain_stat") == mk :
+            list.append(data[i])
+    return list
+
+#프로그램 진입점
+def main2():
+    #검색 질의 요청
+    res = SearchPharmacy("")
+    if(res == None):
+        print("검색 실패!!!")
+        exit()
+    #검색 결과를 json개체로 로딩
+    jres = json.loads(res)
+    if(jres == None):
+        print("json.loads 실패!!!")
+        exit()
+
+    mask_info = searchMK(jres["stores"], 'empty')
+
+    for i in range(len(mask_info)):
+        mask_info[i]
+        print("약국 이름 : ",mask_info[i].get('name'))
+        print("주소 : ",mask_info[i].get('addr'))
+        if mask_info[i].get('created_at')=='empty' :
+            print("정보 갱신 미상")
+        elif mask_info[i].get('created_at') :
+            print("정보 갱신 시각 : ",mask_info[i].get('created_at'))
+        if mask_info[i].get('remain_stat')==None or mask_info[i].get('remain_stat')=='break' or mask_info[i].get('remain_stat')=='empty':
+            print("마스크 재고 : 없음")
+        elif mask_info[i].get('remain_stat')=='some' or mask_info[i].get('remain_stat')=='few':
+            print("마스크 재고 : 부족")
+        elif mask_info[i].get('remain_stat')=='plenty':
+            print("마스크 재고: 충분")
+
+
 # - 위치 기반 별
 #     ( 지정한 위치 기준 반경 500m 이내의 판매 약국을 보여준다)
 
+#  신뢰도 표시 == ??? 재고 입고 시간, 데이터의 정확성 (empty or null), 갱신시각
+
 if __name__ == "__main__" :
     main()
+    main2()
